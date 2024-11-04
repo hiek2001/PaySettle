f2360e0a69740be6f8d7e9a7709d80ac4afc6789
RANNY <hiek2001@naver.com>
2024-11-05 00:44:16 +0900

    style: 불필요한 파일 삭제 및 코드 포멧팅

--- revenue-service/src/main/java/com/project/revenueservice/RevenueServiceApplication.java
+++ revenue-service/src/main/java/com/project/revenueservice/RevenueServiceApplication.java
@@ -1,6 +1,7 @@
 package com.project.revenueservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = {
 		"com.project.revenueservice.repository"
 })
--- streaming-service/src/main/java/com/project/streamingservice/controller/VideoController.java
+++ streaming-service/src/main/java/com/project/streamingservice/controller/VideoController.java
@@ -3,24 +3,23 @@ package com.project.streamingservice.controller;
 import com.project.common.exception.RestApiException;
 import com.project.streamingservice.dto.*;
 import com.project.streamingservice.service.StreamingService;
-import com.project.userservice.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "스트리밍 API", description = "동영상 상태 관리(동영상 조회, 등록, 재생 또는 정지, 재생 내역 저장)")
 @RequestMapping("/api/streaming/videos")
 public class VideoController {
-
     private final StreamingService streamingService;
 
     @Operation(summary = "동영상 조회", description = "id를 사용하여 동영상 재생 내역을 저장합니다.", responses = {
