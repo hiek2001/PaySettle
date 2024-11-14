209dafd134f13088f87ff40a8af0584397de8ef9
RANNY <hiek2001@naver.com>
2024-11-14 11:16:38 +0900

    docs: SWAGGER 삭제, Postman API로 대체

--- streaming-service/src/main/java/com/project/streamingservice/controller/VideoController.java
+++ streaming-service/src/main/java/com/project/streamingservice/controller/VideoController.java
@@ -1,13 +1,7 @@
 package com.project.streamingservice.controller;
 
-import com.project.common.exception.RestApiException;
 import com.project.streamingservice.dto.*;
 import com.project.streamingservice.service.StreamingService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +11,15 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "스트리밍 API", description = "동영상 상태 관리(동영상 조회, 등록, 재생 또는 정지, 재생 내역 저장)")
 @RequestMapping("/api/streaming/videos")
 public class VideoController {
     private final StreamingService streamingService;
 
-    @Operation(summary = "동영상 조회", description = "id를 사용하여 동영상 재생 내역을 저장합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "영상 조회 성공", content = @Content(schema = @Schema(implementation = UserHistoryResponseDto.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = RestApiException.class)))
-    })
     @GetMapping("/{videoId}")
     public UserHistoryResponseDto getVideo(@PathVariable Long videoId, @RequestBody UserRequestDto user) {
         return streamingService.createUserVideoHistory(videoId, user);
     }
 
-    @Operation(summary = "동영상 재생 또는 정지", description = "재생 또는 정지 시, 회원의 재생 상태와 내역을 변경합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "재생 또는 정지 성공", content = @Content(schema = @Schema(implementation = UserHistoryResponseDto.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = RestApiException.class)))
-    })
     @PostMapping("/{videoId}/playback")
     public UserHistoryResponseDto playBack(@PathVariable Long videoId, @RequestBody VideoControlReqeustDto reqeustDto) {
        return streamingService.updateVideoPlayback(videoId, reqeustDto);
--- user-service/src/main/java/com/project/userservice/controller/UserController.java
+++ user-service/src/main/java/com/project/userservice/controller/UserController.java
@@ -1,15 +1,9 @@
 package com.project.userservice.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.project.common.exception.RestApiException;
 import com.project.userservice.dto.KakaoLoginResponseDto;
 import com.project.userservice.dto.KakaoLogoutResponseDto;
 import com.project.userservice.service.KakaoService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +18,6 @@ import java.io.IOException;
 @Slf4j()
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "회원 API", description = "카카오 회원가입, 로그인, 로그아웃")
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -43,8 +36,6 @@ public class UserController {
     private String baseUrl;
 
 
-
-    @Operation(summary = "카카오 로그인", description = "가입자 라면 로그인 되고, 미가입자 라면 회원가입이 됩니다.")
     @GetMapping("/login/kakao")
     public void kakaoLogin(@RequestParam String role ,HttpServletResponse response) throws IOException {
         StringBuilder loginUrl = new StringBuilder()
@@ -58,20 +49,11 @@ public class UserController {
         response.sendRedirect(loginUrl.toString());
     }
 
-
-    @Operation(summary = "카카오 로그인 Redirect URL", responses = {
-            @ApiResponse(responseCode = "200", description = "로그인 및 회원가입 성공", content = @Content(schema = @Schema(implementation = KakaoLoginResponseDto.class))),
-            @ApiResponse(responseCode = "400", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = RestApiException.class)))
-    })
     @GetMapping("/login/kakao/redirect")
     public KakaoLoginResponseDto redirectKakaoLogin(@RequestParam String code, @RequestParam String state) throws JsonProcessingException {
         return kakaoService.redirectLogin(code, state);
     }
 
-    @Operation(summary = "카카오 로그아웃" , description = "로그아웃 됩니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "로그아웃 성공", content = @Content(schema = @Schema(implementation = KakaoLogoutResponseDto.class))),
-            @ApiResponse(responseCode = "400", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = RestApiException.class)))
-    })
     @GetMapping("/logout/kakao")
     public KakaoLogoutResponseDto logout(@RequestParam("kakaoToken") String kakaoToken) {
         return kakaoService.redirectLogout(kakaoToken);
