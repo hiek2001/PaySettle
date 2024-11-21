166806b3257de463e7887b0f8ed2e24536a15aba
RANNY <hiek2001@naver.com>
2024-11-21 16:09:21 +0900

    fix: RestAPIException import 오류 수정

---
+++ user-service/src/main/java/com/project/userservice/exception/RestApiException.java
@@ -0,0 +1,11 @@
+package com.project.userservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RestApiException {
+    private String errorMessage;
+    private int statusCode;
+}
--- user-service/src/main/java/com/project/userservice/exception/UserExceptionHandler.java
+++ user-service/src/main/java/com/project/userservice/exception/UserExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.project.userservice.exception;
 
-import com.project.common.exception.RestApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
